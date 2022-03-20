c1b1c15a85bc9966c1d5979a7b877fa5fd0dfe00
Andrei Grigore <andrei.ggr@gmail.com>
2022-03-20 02:55:34 +0200

    dont trigger transaction if inputs are wrong

--- src/config.js
+++ src/config.js
@@ -4,4 +4,11 @@ import { ProxyProvider } from "@elrondnetwork/erdjs/out";
 //https://gateway.elrond.com for main net
 const proxy = new ProxyProvider("https://devnet-gateway.elrond.com", 60000);
 
-export { proxy };
+const bridge = "https://bridge.walletconnect.org";
+
+const webAddress =
+  "erd10ya7tkgjusj6cdhss90awc4lhvv63h6clazsv7w5ska677m2kmsqzgqqzd";
+const maiarAppAddress =
+  "erd14ntyrv83dw72u9jmkl45nww4a0qeyjxlf0dsy4cvkwjetljkrneqzj27d7";
+
+export { proxy, bridge };
